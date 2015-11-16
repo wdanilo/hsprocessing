ca98771fda0111c23dbe6fdf4b805b18e6852fa7
Adam Szlachta <adam.szlachta@gmail.com>
2015-11-16 15:52:19 +0100

    Next draft of slider

--- src/Math/Topology/Geometry/Figures.hs
+++ src/Math/Topology/Geometry/Figures.hs
@@ -11,6 +11,15 @@ newtype Ball a = Ball a deriving (Show, Functor, Foldable, Traversable)
 type Circle a = Dim 2 Ball a
 
 
+-- newtype Hyperrectangle a = Hyperrectangle a deriving (Show, Functor, Foldable, Traversable)
+
+-- type Rectangle a = Dim 2 Rectangle a
+
+
+newtype Halfspace a = Halfspace a deriving (Show, Functor, Foldable, Traversable)
+
+type Halfplane a = Dim 2 Halfspace a
+
 -- TODO: Extend the definition to Regular prism (https://en.wikipedia.org/wiki/Cube)
 data Rect a = Rect a a deriving (Show)
 
